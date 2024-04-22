420433c3edeaeca2db16e8d7e656daec1ad80202
JameelGharra <jameelgharra@gmail.com>
2024-04-22 19:12:27 +0300

    Working towards a standard middleware

---
+++ app/(protected)/settings/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { auth } from '@auth';
+
+async function SettingsPage() {
+    const session = await auth();
+    return (
+        <div>
+            {JSON.stringify(session)}
+        </div>
+    )
+}
+
+export default SettingsPage
---
+++ middleware.ts
@@ -0,0 +1,33 @@
+import authConfig from "@auth.config";
+import NextAuth from "next-auth";
+
+const { auth } = NextAuth(authConfig);
+
+import {
+    DEFAULT_LOGIN_REDIRECT,
+    apiAuthPrefix,
+    publicRoutes,
+    authRoutes,
+} from "@routes";
+
+export default auth((req) => {
+    const { nextUrl } = req;
+    const isLoggedIn = !!req.auth;
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if(isApiAuthRoute) {
+        return ;
+    }
+    if(isAuthRoute) {
+        if(isLoggedIn) {
+            return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+        }
+        return ;
+    }
+})
+
+export const config = {
+    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
