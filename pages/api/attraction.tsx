cb138b1b18c695f70b3a445b7673948ebf1c343e
kfir41 <kfirdayan41@gmail.com>
2024-04-26 16:25:08 +0300

    daniel hagever

--- pages/api/attraction.tsx
+++ pages/api/attraction.tsx
@@ -4,7 +4,6 @@ import {connectToDB} from '../../utils/database';
 import mongoose from 'mongoose';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
     await connectToDB();
     console.log("Connected to the database successfully.");
 }
