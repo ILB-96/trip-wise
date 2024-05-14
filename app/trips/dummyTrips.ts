43c15070ffdcdcab64feccf6545c69dedb2faeb8
Israel Barmack <israel.bar.dev@gmail.com>
2024-05-14 12:53:21 +0300

    Added trips main page

---
+++ app/trips/dummyTrips.ts
@@ -0,0 +1,96 @@
+import { dummyData } from "@app/attractions/page";
+import { TripType } from "./page";
+
+export const dummyTrips: TripType[] = [
+  {
+    name: "Tsukiji Fish Market",
+    rating: 3,
+    views: 1000,
+    country: "Japan",
+    days: [
+      {
+        date: "2022-01-01",
+        visits: [
+          {
+            attraction: dummyData[0],
+            from_time: "9:00 AM",
+            to_time: "11:00 AM",
+          },
+          {
+            attraction: dummyData[1],
+            from_time: "12:00 PM",
+            to_time: "4:00 PM",
+          },
+        ],
+      },
+    ],
+    description: "The world's largest fish market.",
+    image: "https://source.unsplash.com/400x300/?tsukiji",
+  },
+  {
+    name: "Historical Europe",
+    rating: 4.7,
+    views: 1500,
+    country: "Germany",
+    days: [
+      {
+        date: "2022-05-15",
+        visits: [
+          {
+            attraction: dummyData[3], // The Eiffel Tower
+            from_time: "10:00 AM",
+            to_time: "1:00 PM",
+          },
+          {
+            attraction: dummyData[6], // The Colosseum
+            from_time: "3:00 PM",
+            to_time: "6:00 PM",
+          },
+        ],
+      },
+      {
+        date: "2022-05-16",
+        visits: [
+          {
+            attraction: dummyData[5], // The Great Wall of China
+            from_time: "9:00 AM",
+            to_time: "3:00 PM",
+          },
+        ],
+      },
+    ],
+    description: "Explore the rich history of Europe's iconic landmarks.",
+    image: "https://source.unsplash.com/400x300/?europe,landmarks",
+  },
+  {
+    name: "Adventure in South America",
+    rating: 4.3,
+    views: 950,
+    country: "Peru",
+    days: [
+      {
+        date: "2022-09-10",
+        visits: [
+          {
+            attraction: dummyData[7], // Machu Picchu
+            from_time: "5:00 AM",
+            to_time: "10:00 AM",
+          },
+        ],
+      },
+      {
+        date: "2022-09-12",
+        visits: [
+          {
+            attraction: dummyData[8], // The Statue of Liberty
+            from_time: "1:00 PM",
+            to_time: "4:00 PM",
+          },
+        ],
+      },
+    ],
+    description:
+      "A breathtaking adventure through the mountains and jungles of South America.",
+    image: "https://source.unsplash.com/400x300/?machu,picchu",
+  },
+];
---
+++ components/TripCard.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import StarRating from "@components/StarRating";
+import { useRouter } from "next/navigation";
+import { TripType } from "@app/trips/page";
+import { Eye } from "lucide-react";
+
+const TripCard = (trip: TripType) => {
+  const router = useRouter();
+
+  const navigateToTripDetail = () => {
+    router.push("/itinerary"); // Navigate to the trip detail page
+  };
+
+  return (
+    <>
+      <div
+        className="rounded-lg overflow-hidden shadow-xl transform transition-transform hover:scale-[102%] cursor-pointer relative"
+        onClick={navigateToTripDetail}
+      >
+        <div className="flex items-center">
+          {trip.image && (
+            <Image
+              className="w-full h-48 object-cover"
+              width={500}
+              height={300}
+              src={trip.image}
+              alt={trip.name}
+              priority
+            />
+          )}
+        </div>
+        <div className="p-4">
+          <h2 className="font-semibold text-xl">{trip.name}</h2>
+          {trip.views && (
+            <Badge
+              className={
+                (trip.price ? "top-8" : "top-2") +
+                " flex absolute left-2 text-green-600 px-1 py-0 transform transition-transform hover:scale-105 cursor-pointer"
+              }
+            >
+              <Eye className="w-4 h-4 mr-1" />
+              {trip.views}
+            </Badge>
+          )}
+          {trip.rating && <StarRating rating={trip.rating} />}
+          <p className="text-gray-600">{trip.country}</p>
+          <p className="text-gray-800">{trip.description}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TripCard;
