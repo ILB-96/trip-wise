f4e71168c38e6b9a53314dec658bdd5b65b8db98
Israel Barmack <israel.bar.dev@gmail.com>
2024-05-17 13:08:14 +0300

    removed Community page

--- constants/navigation.tsx
+++ constants/navigation.tsx
@@ -1,22 +1,17 @@
 export const navigation = [
-    {
-        id: "0",
-        title: "HOME",
-        url: "/",
-    },
-    {
-        id: "1",
-        title: "TRIPS",
-        url: "/trips",
-    },
-    {
-        id: "2",
-        title: "ATTRACTIONS",
-        url: "/attractions",
-    },
-    {
-        id: "3",
-        title: "COMMUNITY",
-        url: "/community",
-    },
-]
+  {
+    id: "0",
+    title: "HOME",
+    url: "/",
+  },
+  {
+    id: "1",
+    title: "TRIPS",
+    url: "/trips",
+  },
+  {
+    id: "2",
+    title: "ATTRACTIONS",
+    url: "/attractions",
+  },
+];
