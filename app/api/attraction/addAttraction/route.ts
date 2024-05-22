e9141d390bdccf0d713af729d2db8920b73122b9
kfir41 <kfirdayan41@gmail.com>
2024-05-22 16:15:19 +0300

    put dummyAttraction in mongo

--- app/api/attraction/addAttraction/route.ts
+++ app/api/attraction/addAttraction/route.ts
@@ -2,6 +2,7 @@ import {connectToDB} from "@utils/database";
 import Attraction from "@models/attraction";
 import {NextResponse} from "@node_modules/next/server";
 import {addAttraction} from "@lib/attraction";
+import attraction from "@models/attraction";
 
 export async function POST(request: Request ) {
     try {
@@ -16,4 +17,23 @@ export async function POST(request: Request ) {
         );
     }
 }
+// export async function POST(request: Request ) {
+//     await connectToDB();
+//     try {
+//         const promises = dummyData.map(async (attraction) => {
+//             await Attraction.updateOne(
+//                 { title: attraction.title }, // Find the document with the same title
+//                 { $set: attraction }, // Update with new data
+//                 { upsert: true } // Insert if it doesn't exist
+//             );
+//         });
+//
+//         await Promise.all(promises);
+//
+//         return NextResponse.json({ message: 'Attractions added/updated successfully' }, { status: 200 });
+//     } catch (error) {
+//         return NextResponse.json({ error: `Error adding attractions: ${error.message}` }, { status: 500 });
+//     }
+// }
+
 
