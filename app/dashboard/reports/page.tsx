d6021c871e5b111ded526c98b733bcb4a89a7226
Israel Barmack <israel.bar.dev@gmail.com>
2024-05-26 08:52:40 +0300

    add report dashboard page

---
+++ app/dashboard/reports/page.tsx
@@ -0,0 +1,56 @@
+// pages/reports.tsx
+import React from "react";
+import { getReports } from "@lib/reports";
+import Pagination from "@components/dashboard/pagination/Pagination";
+import SearchComp from "@components/dashboard/search/Search";
+import styles from "@components/dashboard/users/users.module.css";
+import ReportItem from "@components/dashboard/reports/ReportItem";
+import {
+  Table,
+  TableBody,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@components/ui/table";
+
+interface SearchParams {
+  q?: string;
+  page?: number;
+}
+
+interface ReportsPageProps {
+  searchParams: SearchParams;
+}
+
+const ReportsPage: React.FC<ReportsPageProps> = async ({ searchParams }) => {
+  const q = searchParams?.q || "";
+  const page = parseInt(searchParams?.page as string, 10) || 1;
+
+  const { count, reports } = await getReports(q, page);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <SearchComp placeholder="Search for a report..." />
+      </div>
+      <Table className={styles.table}>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Comment</TableHead>
+            <TableHead>Snitch</TableHead>
+            <TableHead>Reason</TableHead>
+            <TableHead>Actions</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {reports.map((report) => (
+            <ReportItem key={report._id} report={report} />
+          ))}
+        </TableBody>
+      </Table>
+      <Pagination count={count} />
+    </div>
+  );
+};
+
+export default ReportsPage;
---
+++ lib/reports.ts
@@ -0,0 +1,79 @@
+import { connectToDB } from "@utils/database";
+import TripCommentReport, {
+  ITripCommentReport,
+  ITripCommentReportBase,
+} from "@models/tripCommentReport";
+import { revalidatePath } from "next/cache";
+import TripComment from "@models/tripComment";
+
+export const REPORTS_PER_PAGE = 2;
+
+export const getReports = async (q: string | RegExp, page: number) => {
+  const regex = new RegExp(q, "i");
+  try {
+    await connectToDB(); // Ensure the database connection is awaited
+
+    const count = await TripCommentReport.find({
+      reason: { $regex: regex },
+    }).countDocuments();
+
+    const reports = await TripCommentReport.find({ reason: { $regex: regex } })
+      .populate("tripCommentId") // Populate the tripCommentId field
+      .populate("snitchId") // Populate the snitchId field
+      .limit(REPORTS_PER_PAGE)
+      .skip(REPORTS_PER_PAGE * (page - 1))
+      .lean();
+
+    reports.forEach((report) => {
+      report._id = report._id.toString();
+      report._id = report.tripCommentId._id.toString();
+      report._id = report.snitchId._id.toString();
+    });
+    return { count, reports };
+  } catch (err) {
+    throw new Error("Failed to fetch reports!");
+  }
+};
+
+export const deleteTripCommentReport = async (
+  formData: Iterable<readonly [PropertyKey, any]>
+) => {
+  const { tripCommentId, id } = Object.fromEntries(formData);
+
+  try {
+    connectToDB();
+    await TripComment.findByIdAndDelete(tripCommentId);
+    await TripCommentReport.findByIdAndDelete(id);
+  } catch (err) {
+    console.log(err);
+    throw new Error("Failed to delete comment!");
+  }
+
+  revalidatePath("/dashboard/reports");
+};
+
+export const addReport = async (
+  report: ITripCommentReportBase
+): Promise<{
+  success: boolean;
+  error?: string;
+  report?: ITripCommentReportBase;
+}> => {
+  try {
+    await connectToDB();
+    const newReport: ITripCommentReport = await TripCommentReport.create({
+      ...report,
+    });
+    if (!newReport)
+      return {
+        success: false,
+        error: "Was not able to create the report in DB",
+      };
+
+    revalidatePath(`/dashboard/reports`);
+
+    return { success: true, report: newReport };
+  } catch (error: any) {
+    return { success: false, error: error.message };
+  }
+};
