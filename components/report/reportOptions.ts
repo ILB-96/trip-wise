cf475ef5baae38508056eb90516f5d039d622b40
Israel Barmack <israel.bar.dev@gmail.com>
2024-05-27 17:25:54 +0300

    report button component

---
+++ components/report/reportOptions.ts
@@ -0,0 +1,11 @@
+export const tripReportOptions = [
+  "Spam or advertising",
+  "Harassment or bullying",
+  "Hate speech or offensive content",
+  "Violence or dangerous behavior",
+  "Misinformation or false information",
+  "Inappropriate content",
+  "Copyright infringement",
+  "Privacy violation",
+  "Other",
+];
