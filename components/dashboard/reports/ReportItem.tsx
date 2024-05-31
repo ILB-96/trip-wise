35e99f6d59d869cbdad9f9e53cb0753652fbc610
Israel Barmack <israel.bar.dev@gmail.com>
2024-05-31 12:56:12 +0300

    chore: Update table header label from "Snitch" to "Author"

--- components/dashboard/reports/ReportItem.tsx
+++ components/dashboard/reports/ReportItem.tsx
@@ -1,22 +1,35 @@
 import React from "react";
-import { deleteTripCommentReport } from "@lib/reports";
-import { Trash2, View } from "lucide-react";
+import { deleteTripCommentReport, deleteTripReport } from "@lib/reports";
+import { Trash2, View, SquareCheck } from "lucide-react";
 import styles from "@components/dashboard/users/users.module.css";
 import { Button } from "@components/ui/button";
 import { TableCell, TableRow } from "@components/ui/table";
-
-const ReportItem = ({ report }) => {
+import Link from "next/link";
+const ReportItem = ({ report }: { report: any }) => {
   return (
     <TableRow>
       <TableCell>
         {report.tripCommentId ? report.tripCommentId.content : "N/A"}
       </TableCell>
       <TableCell className="max-sm:hidden">
-        {report.snitchId ? `${report.snitchId.email}` : "N/A"}
+        {report.tripCommentId.author
+          ? `${report.tripCommentId.author.email}`
+          : "N/A"}
       </TableCell>
       <TableCell className="max-sm:hidden">{report.reason}</TableCell>
       <TableCell>
         <div className={styles.buttons}>
+          <Link href={`/trip/${report.tripCommentId.tripId._id}`}>
+            <Button variant="outline">
+              <View />
+            </Button>
+          </Link>
+          <form action={deleteTripReport}>
+            <input type="hidden" name="id" value={report._id} />
+            <Button className="bg-green-500" variant="ghost">
+              <SquareCheck />
+            </Button>
+          </form>
           <form action={deleteTripCommentReport}>
             <input type="hidden" name="id" value={report._id} />
             <input
